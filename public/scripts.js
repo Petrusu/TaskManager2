b4ac2f236ffba9950b569f209f0662e4ea2e08a1
Petrusu <sofya.shikhova@gmail.com>
2024-07-05 13:30:27 +0300

    commit

--- public/scripts.js
+++ public/scripts.js
@@ -4,43 +4,64 @@ let tasksByStage = {
     review: [],
     done: []
 };
-fetch('http://localhost:3000/api/v1/tasks')
-    .then(response => {
+let stages = [];
+
+async function fetchStages() {
+    try {
+        const response = await fetch('http://localhost:3000/api/v1/stages');
+        const data = await response.json();
+        stages = data.stages;
+    } catch (error) {
+        console.error('Error fetching stages:', error);
+        stages = [];
+    }
+}
+async function fetchTasksAndRender() {
+    try {
+        const response = await fetch('http://localhost:3000/api/v1/tasks');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json();
-    })
-    .then(data => {
+        const data = await response.json();
+
+        // Очистка объектов для каждой стадии
+        tasksByStage.ready = [];
+        tasksByStage.progress = [];
+        tasksByStage.review = [];
+        tasksByStage.done = [];
+
         // Распределение заданий по массивам в зависимости от стадии
         data.tasks.forEach(task => {
-            switch (task.stage) {
-                case '665c6ed00e70091d14a276dd': // ID стадии "ready"
-                    tasksByStage.ready.push(task);
-                    break;
-                case '665c6ed00e70091d14a276de': // ID стадии "progress"
-                    tasksByStage.progress.push(task);
-                    break;
-                case '665c6ed00e70091d14a276df': // ID стадии "review"
-                    tasksByStage.review.push(task);
-                    break;
-                case '665c6ed00e70091d14a276e0': // ID стадии "done"
-                    tasksByStage.done.push(task);
-                    break;
-                default:
-                    // Обработка случаев, если стадия не соответствует ожидаемым значениям
-                    break;
+            const stage = stages.find(stage => stage._id === task.stage);
+            if (stage) {
+                switch (stage.name) {
+                    case 'ready':
+                        tasksByStage.ready.push(task);
+                        break;
+                    case 'progress':
+                        tasksByStage.progress.push(task);
+                        break;
+                    case 'review':
+                        tasksByStage.review.push(task);
+                        break;
+                    case 'done':
+                        tasksByStage.done.push(task);
+                        break;
+                    default:
+                        // Обработка случаев, если стадия не соответствует ожидаемым значениям
+                        break;
+                }
             }
         });
 
         // Вызываем функцию для отображения задач на странице
         renderTasksOnPage();
-    })
-    .catch(error => {
+    } catch (error) {
         console.error('Fetch error:', error);
         // Обработка ошибок при запросе
-    });
-
+    }
+}
+fetchStages().then(fetchTasksAndRender);
 // Функция для отображения задач на странице
 function renderTasksOnPage() {
     Object.keys(tasksByStage).forEach(stage => {
@@ -117,7 +138,8 @@ function renderTasksOnPage() {
 document.getElementById('cancel-delete-btn').addEventListener('click', () => {
     document.getElementById('delete-confirmation-modal').classList.add('hidden');
 });
-//Удаление
+
+// Удаление
 document.getElementById('confirm-delete-btn').addEventListener('click', () => {
     const deleteModal = document.getElementById('delete-confirmation-modal');
     const taskId = deleteModal.dataset.taskId;
@@ -126,7 +148,7 @@ document.getElementById('confirm-delete-btn').addEventListener('click', () => {
     }).then(response => {
         if (response.ok) {
             deleteModal.classList.add('hidden');
-            renderTasksOnPage(); // Обновляем список задач после удаления
+            fetchTasksAndRender(); // Обновляем список задач после удаления
         } else {
             console.error('Failed to delete task:', response.statusText);
         }
@@ -134,36 +156,66 @@ document.getElementById('confirm-delete-btn').addEventListener('click', () => {
         console.error('Error deleting task:', error);
     });
 });
-//Редактирование
-document.getElementById('edit-task-form').addEventListener('submit', (event) => {
+
+// Редактирование
+document.getElementById('edit-task-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     const editTaskModal = document.getElementById('edit-task-modal');
     const taskId = editTaskModal.dataset.taskId;
-    const updatedTask = {
+    const dataValue = document.getElementById("planned-completion-date").value;
+    const expiredDate = new Date(dataValue);
+
+    const newTask = {
         title: document.getElementById('task-title').value,
         stage: document.getElementById('task-stage').value,
-        expiredDate: document.getElementById('planned-completion-date').value,
+        expiredDate: expiredDate.valueOf(),
         value: document.getElementById('task-description').value,
         completeProgress: document.getElementById('task-progress').value
     };
 
-    fetch(`http://localhost:3000/api/v1/tasks/${taskId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedTask)
-    }).then(response => {
-        if (response.ok) {
-            editTaskModal.classList.add('hidden');
-            fetchTasksAndRender(); // Обновляем список задач после редактирования
-        } else {
-            console.error('Failed to update task:', response.statusText);
+    console.log('Task to be saved:', newTask);
+
+    if (taskId) {
+        // Редактирование задачи
+        try {
+            const response = await fetch(`http://localhost:3000/api/v1/tasks/${taskId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            });
+            if (response.ok) {
+                editTaskModal.classList.add('hidden');
+                fetchTasksAndRender();
+            } else {
+                console.error('Failed to update task:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error updating task:', error);
         }
-    }).catch(error => {
-        console.error('Error updating task:', error);
-    });
+    } else {
+        // Добавление новой задачи
+        try {
+            const response = await fetch('http://localhost:3000/api/v1/tasks', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            });
+            if (response.ok) {
+                editTaskModal.classList.add('hidden');
+                fetchTasksAndRender();
+            } else {
+                console.error('Failed to add task:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Error adding task:', error);
+        }
+    }
 });
+
 
 document.getElementById('close-modal-btn').addEventListener('click', () => {
     document.getElementById('edit-task-modal').classList.add('hidden');
@@ -173,31 +225,34 @@ document.getElementById('task-progress').addEventListener('input', (event) => {
     document.getElementById('progress-value').innerText = `${event.target.value}%`;
 });
 
-//Добавление
-document.getElementById('add-task-btn').addEventListener('click', function() {
-    // Минимальный набор данных для создания задачи
-    const taskData = {
-        title: 'Task Manager',
-        value: 'Текст таска',
-        expiredDate: new Date().valueOf(),
-        stage: '665c6ed00e70091d14a276dd'
-    };
+// Добавление
+document.getElementById('add-task-btn').addEventListener('click', () => {
+    const editTaskModal = document.getElementById('edit-task-modal');
+    editTaskModal.dataset.taskId = ''; // Убрать id задачи, если это новое добавление
+    editTaskModal.classList.remove('hidden');
+    document.getElementById('edit-task-form').reset();
+    document.getElementById('progress-value').innerText = '0%';
 
-    fetch('http://localhost:3000/api/v1/tasks', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(taskData)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-            renderTasksOnPage(); // Обновляем список задач после добавления
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    const taskStageSelect = document.getElementById('task-stage');
+    taskStageSelect.innerHTML = ''; // Очистить старые опции
+
+    stages.forEach(stage => {
+        const option = document.createElement('option');
+        option.value = stage._id;
+        option.text = stage.name.charAt(0).toUpperCase() + stage.name.slice(1);
+        taskStageSelect.appendChild(option);
+    });
+
+    console.log('Stages initialized for new task:', stages);
+});
+
+
+document.getElementById('close-modal-btn').addEventListener('click', () => {
+    document.getElementById('edit-task-modal').classList.add('hidden');
+});
+
+document.getElementById('task-progress').addEventListener('input', (event) => {
+    document.getElementById('progress-value').innerText = `${event.target.value}%`;
 });
 
 
